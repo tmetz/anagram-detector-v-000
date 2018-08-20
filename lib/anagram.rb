8b5637cb9e91dbdda68d52e105c0920ff5206614
Tammy Metz <tammy@tammymetz.com>
2018-08-20 22:57:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :match_word
+  def initialize(word)
+    self.match_word = word
+  end
+
+  def match(word_array)
+    return_array = []
+    match_word_array = self.match_word.split("").sort
+    word_array.each do |each_word|
+      if match_word_array == each_word.split("").sort
+        return_array << each_word
+      end
+    end
+    return_array
+  end
+end
